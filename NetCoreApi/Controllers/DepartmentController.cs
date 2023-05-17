53ef6877be83fa633400c4ac9c3405ce7301aada
Zouhair.ETTARAK <ettarak.zouhair@gmail.com>
2023-05-17 17:06:51 +0100

    asp net core api - get method ( department)

---
+++ NetCoreApi/Controllers/DepartmentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT DepartmentId, DepartmentName FROM dbo.Department ORDER BY 1 DESC";
+            string sqlDataSource = _configuration.GetConnectionString("MyConnectionString");
+
+            DataTable dataTable = new DataTable();
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+            return new JsonResult(dataTable);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @"SELECT DepartmentId, DepartmentName FROM dbo.Department WHERE DepartmentId = @DepartmentId";
+            string sqlDataSource = _configuration.GetConnectionString("MyConnectionString");
+
+            DataTable dataTable = new DataTable();
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("DepartmentId", id);
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+            return new JsonResult(dataTable);
+        }
+    }
+}
---
+++ NetCoreApi/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreApi.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public string? DepartmentName { get; set; }
+    }
+}
